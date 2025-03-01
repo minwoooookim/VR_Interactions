53441091857b8de2c1d9e8e24f0b0e4b343a6768
minwoooookim <rlaalsdn4560@gmail.com>
2025-03-01 23:07:07 +0900

    키보드 테스트 스크립트 수정
    - words.txt 파일들 단어 구성은 아래와 같음
    - 6자 이하 단어 개수: 236
    - 6자 초과 단어 개수: 234
    - 위 개수 분류에 따라 6자를 기준으로 단어를 나누어 출력하도록 수정

--- Assets/Scrpts/TypingTest.cs
+++ Assets/Scrpts/TypingTest.cs
@@ -10,8 +10,8 @@ public class TypingTest : MonoBehaviour
     [SerializeField] private TMP_InputField inputField;   // 사용자가 타이핑하는 InputField
 
     private List<string> allWords = new List<string>();      // words.txt에서 읽어온 모든 단어
-    private List<string> shortWords = new List<string>();      // 길이가 5자 이하인 단어들
-    private List<string> longWords = new List<string>();       // 길이가 5자 초과인 단어들
+    private List<string> shortWords = new List<string>();      // 길이가 6자 이하인 단어들
+    private List<string> longWords = new List<string>();       // 길이가 6자 초과인 단어들
 
     private void Start()
     {
@@ -26,7 +26,7 @@ public class TypingTest : MonoBehaviour
             // 단어들을 길이에 따라 분류하기
             foreach (string word in allWords)
             {
-                if (word.Length <= 5)
+                if (word.Length <= 6)
                 {
                     shortWords.Add(word);
                 }
@@ -75,8 +75,8 @@ public class TypingTest : MonoBehaviour
             // 이전 단어 가져오기
             string previousWord = testWordText.text;
 
-            // 이전 단어가 5자 이하이면 longWords에서 선택
-            if (previousWord.Length <= 5)
+            // 이전 단어가 6자 이하이면 longWords에서 선택
+            if (previousWord.Length <= 6)
             {
                 if (longWords.Count > 0)
                 {
@@ -85,11 +85,11 @@ public class TypingTest : MonoBehaviour
                 }
                 else
                 {
-                    Debug.LogWarning("5자 초과 단어가 없습니다.");
+                    Debug.LogWarning("6자 초과 단어가 없습니다.");
                     nextWord = previousWord; // 대체 로직
                 }
             }
-            // 이전 단어가 5자 초과이면 shortWords에서 선택
+            // 이전 단어가 6자 초과이면 shortWords에서 선택
             else
             {
                 if (shortWords.Count > 0)
@@ -99,7 +99,7 @@ public class TypingTest : MonoBehaviour
                 }
                 else
                 {
-                    Debug.LogWarning("5자 이하 단어가 없습니다.");
+                    Debug.LogWarning("6자 이하 단어가 없습니다.");
                     nextWord = previousWord; // 대체 로직
                 }
             }
